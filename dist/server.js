8b33f72298489a84b31894dab37e5c4b94718b49
rana-arju <arjurana20@gmail.com>
2025-02-19 19:12:30 +0600

    update

--- dist/server.js
+++ dist/server.js
@@ -13,47 +13,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-const logger_1 = require("./app/shared/logger");
-const prisma_1 = __importDefault(require("./app/shared/prisma"));
+const client_1 = require("@prisma/client");
+const logger_1 = __importDefault(require("./app/shared/logger"));
+const prisma = new client_1.PrismaClient();
 const port = process.env.PORT || 3000;
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            yield prisma_1.default.$connect();
-            logger_1.logger.info("Database connected successfully");
+            yield prisma.$connect();
+            logger_1.default.info("Database connected successfully");
             if (process.env.VERCEL) {
                 // For Vercel serverless deployment
                 module.exports = app_1.default;
             }
             else {
                 // For local development
-                const server = app_1.default.listen(port, () => {
-                    logger_1.logger.info(`Server is running on port ${port}`);
-                });
-                const exitHandler = () => {
-                    if (server) {
-                        server.close(() => {
-                            logger_1.logger.info("Server closed");
-                        });
-                    }
-                    process.exit(1);
-                };
-                const unexpectedErrorHandler = (error) => {
-                    logger_1.logger.error(error);
-                    exitHandler();
-                };
-                process.on("uncaughtException", unexpectedErrorHandler);
-                process.on("unhandledRejection", unexpectedErrorHandler);
-                process.on("SIGTERM", () => {
-                    logger_1.logger.info("SIGTERM received");
-                    if (server) {
-                        server.close();
-                    }
+                app_1.default.listen(port, () => {
+                    logger_1.default.info(`Server is running on port ${port}`);
                 });
             }
         }
-        catch (err) {
-            logger_1.logger.error("Unable to connect to the database:", err);
+        catch (error) {
+            logger_1.default.error("Unable to connect to the database:", error);
         }
     });
 }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import type { Server } from "http";
 import app from "./app";
-import config from "./app/config";
 import { PrismaClient } from "@prisma/client";
-import { logger } from "./app/shared/logger";
-import prisma from "./app/shared/prisma";
+import logger from "./app/shared/logger";
+
+const prisma = new PrismaClient();
 const port = process.env.PORT || 3000;
+
 async function main() {
   try {
     await prisma.$connect();
@@ -15,36 +15,12 @@ async function main() {
       module.exports = app;
     } else {
       // For local development
-      const server: Server = app.listen(port, () => {
+      app.listen(port, () => {
         logger.info(`Server is running on port ${port}`);
       });
-
-      const exitHandler = () => {
-        if (server) {
-          server.close(() => {
-            logger.info("Server closed");
-          });
-        }
-        process.exit(1);
-      };
-
-      const unexpectedErrorHandler = (error: unknown) => {
-        logger.error(error);
-        exitHandler();
-      };
-
-      process.on("uncaughtException", unexpectedErrorHandler);
-      process.on("unhandledRejection", unexpectedErrorHandler);
-
-      process.on("SIGTERM", () => {
-        logger.info("SIGTERM received");
-        if (server) {
-          server.close();
-        }
-      });
     }
-  } catch (err) {
-    logger.error("Unable to connect to the database:", err);
+  } catch (error) {
+    logger.error("Unable to connect to the database:", error);
   }
 }
 
