1b5a66a8e7b7b77bf565bd1ee0b2df3eacb3ee2f
rana-arju <arjurana20@gmail.com>
2025-03-17 13:33:44 +0600

    error handling zod modify

--- dist/app/error/handleZodError.js
+++ dist/app/error/handleZodError.js
@@ -1,16 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const handleZodError = (error) => {
-    const errorMessages = error.issues.map((issuse) => {
-        return {
-            path: issuse?.path[issuse.path.length - 1],
-            message: issuse?.message,
-        };
-    });
+    const errorMessages = error.issues.map((issue) => ({
+        path: issue.path[issue.path.length - 1],
+        message: issue.message,
+    }));
     const statusCode = 400;
     return {
         statusCode,
-        message: "validation error",
+        message: "Validation error",
         errorMessages,
     };
 };
--- src/app/error/handleZodError.ts
+++ src/app/error/handleZodError.ts
@@ -1,18 +1,17 @@
-import { ZodError, ZodIssue } from "zod";
+import { ZodError } from "zod";
 import { IErrorSources, IGenericErrorResponse } from "../interface/error";
 
 const handleZodError = (error: ZodError): IGenericErrorResponse => {
-  const errorMessages: IErrorSources = error.issues.map((issuse: ZodIssue) => {
-    return {
-      path: issuse?.path[issuse.path.length - 1],
-      message: issuse?.message,
-    };
-  });
+  const errorMessages: IErrorSources = error.issues.map((issue) => ({
+    path: issue.path[issue.path.length - 1],
+    message: issue.message,
+  }));
+
   const statusCode = 400;
   return {
     statusCode,
-    message: "validation error",
-     errorMessages,
+    message: "Validation error",
+    errorMessages,
   };
 };
 
